2b3e107609d3fb669f3515882110ee35aaff6e03
Rishikesh Dongre <104708547+rishidongregitub@users.noreply.github.com>
2024-09-13 14:03:22 +0000

    starting problem solving

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // Treat all JS CODE as Newer Version
+
+// alert("Hello") we are using nodeJS,Not Browser
+
+console.log(3
+    +
+    3); // code readability should be high
+
+    console.log("Rishi")
+
+
+    let name = "Rishi" // String
+    let age  = 27 // Number
+    let isLoggedIn = true // boolean
+
+    // Number
+    // Bigint 
+    // String => ""
+    // Boolean  => true or false 
+    // Null => Standalone Value (Its the represent of empty value)
+    // Undefined => not assign value
+    // Symbol => Unique
+
+    // Object  
